2de41152a35cd0357f5f9ed058de35f50e501c83
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2024-01-11 09:12:11 +0700

    edit SetoranData.kt

--- app/src/main/java/com/example/tahfizhhub/model/SetoranData.kt
+++ app/src/main/java/com/example/tahfizhhub/model/SetoranData.kt
@@ -2,10 +2,10 @@ package com.example.tahfizhhub.model
 
 
 data class SetoranData(
-    var setoranID: String = "",
-    var juz: String = "",
-    var surat: String = "",
+    val setoranID: String = "",
+    val juz: String = "",
+    val surat: String = "",
     val ayat: String = "",
-    var halaman: String = "",
-    var timestamp: Any? = null
+    val halaman: String = "",
+    val timestamp: Any? = null
 )
