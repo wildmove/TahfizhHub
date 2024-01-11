dede81b95d005695dbef477c910680c7a7d8185b
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2024-01-11 22:57:09 +0700

    Add MurajaahData.kt

---
+++ app/src/main/java/com/example/tahfizhhub/model/MurajaahData.kt
@@ -0,0 +1,10 @@
+package com.example.tahfizhhub.model
+
+data class MurajaahData(
+    var setoranID: String = "",
+    val juz: String = "",
+    val surat: String = "",
+    val ayat: String = "",
+    val halaman: String = "",
+    var timestamp: Any? = null
+)
