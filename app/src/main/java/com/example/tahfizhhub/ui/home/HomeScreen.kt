eab01d3c42f2f74fce74bf35e3d6692b73b45e1e
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2024-01-11 10:29:45 +0700

    Add HomeScreen.kt

---
+++ app/src/main/java/com/example/tahfizhhub/ui/home/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.tahfizhhub.ui.home
+
+import com.example.tahfizhhub.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "setoran"
+}
