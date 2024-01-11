485155bf677c30eb3f59b613ea602173d197c315
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2024-01-11 21:00:08 +0700

    Melengkapi EditSetoranScreen

--- app/src/main/java/com/example/tahfizhhub/ui/edit/EditSetoranScreen.kt
+++ app/src/main/java/com/example/tahfizhhub/ui/edit/EditSetoranScreen.kt
@@ -1,7 +1,20 @@
 package com.example.tahfizhhub.ui.edit
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tahfizhhub.navigasi.DestinasiNavigasi
+import com.example.tahfizhhub.ui.PenyediaViewModel
+import com.example.tahfizhhub.ui.TahfizhTopAppBar
+import com.example.tahfizhhub.ui.add.AddBody
+import kotlinx.coroutines.launch
 
 object EditSetoranDestination : DestinasiNavigasi {
     override val route = "item_edit_setoran"
@@ -10,10 +23,39 @@ object EditSetoranDestination : DestinasiNavigasi {
     val routeWithArgs = "${EditSetoranDestination.route}/{$setoranId}"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun EditSetoranScreen(
     navigateBack: () -> Unit,
     onNavigateUp: () -> Unit,
     modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            TahfizhTopAppBar(
+                title = EditSetoranDestination.titleRes,
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        AddBody(
+            addUIState = viewModel.setoranUiState,
+            onSetoranValueChange = viewModel::updateUIState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSetoran()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+            )
 
+    }
 }
