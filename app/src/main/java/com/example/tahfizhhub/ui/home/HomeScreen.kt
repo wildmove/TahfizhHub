a43a00c67a6e307f115b846493efa33a1f7936c5
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2024-01-11 10:48:26 +0700

    add fun DataSetoran in HomeScreen.kt

--- app/src/main/java/com/example/tahfizhhub/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/tahfizhhub/ui/home/HomeScreen.kt
@@ -1,6 +1,22 @@
 package com.example.tahfizhhub.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.tahfizhhub.model.SetoranData
 import com.example.tahfizhhub.navigasi.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi {
@@ -13,4 +29,42 @@ fun HomeScreen(
     navigateToItemEntry: () -> Unit
 ) {
 
+}
+@Composable
+fun DataSetoran(
+    setoranData: SetoranData,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = setoranData.surat,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+
+                Text(
+                    text = setoranData.halaman,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(
+                text = setoranData.juz,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
 }
