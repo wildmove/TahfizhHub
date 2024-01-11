21046742c7eddd8153c80182d354aa7bb00c87ae
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2024-01-11 10:20:43 +0700

    Add TahfizhApplication.kt

---
+++ app/src/main/java/com/example/tahfizhhub/TahfizhApplication.kt
@@ -0,0 +1,15 @@
+package com.example.tahfizhhub
+
+import android.app.Application
+import com.example.tahfizhhub.repository.AppContainer
+import com.example.tahfizhhub.repository.SetoranContainer
+
+class TahfizhApplication : Application() {
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+
+        container = SetoranContainer()
+    }
+}
