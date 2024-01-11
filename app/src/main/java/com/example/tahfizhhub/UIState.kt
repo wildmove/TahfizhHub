f21ecfdfc5684af1c99835e3247f661d95668b24
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2024-01-11 10:18:24 +0700

    Add UIState.kt

---
+++ app/src/main/java/com/example/tahfizhhub/UIState.kt
@@ -0,0 +1,48 @@
+package com.example.tahfizhhub
+
+import com.example.tahfizhhub.model.SetoranData
+
+data class AddUIState(
+    val addEvent: AddEvent = AddEvent(),
+)
+
+data class AddEvent(
+    var setoranID: String = "",
+    var juz: String = "",
+    var surat: String = "",
+    val ayat: String = "",
+    var halaman: String = "",
+    var timestamp: Any? = null
+)
+
+fun AddEvent.toSetoranData() = SetoranData(
+    setoranID = setoranID,
+    juz = juz,
+    surat = surat,
+    ayat = ayat,
+    halaman = halaman,
+    timestamp = timestamp
+)
+
+data class DetailUIState(
+    val addEvent: AddEvent = AddEvent()
+)
+
+fun SetoranData.toDetailSetoranData(): AddEvent =
+    AddEvent(
+        setoranID = setoranID,
+        juz = juz,
+        surat = surat,
+        ayat = ayat,
+        halaman = halaman,
+        timestamp = timestamp
+    )
+
+fun SetoranData.toUIStateSetoranData(): AddUIState = AddUIState(
+    addEvent = this.toDetailSetoranData()
+)
+
+data class HomeUIState(
+    val listSetoran: List<SetoranData> = listOf(),
+    val dataLength: Int = 0
+)
