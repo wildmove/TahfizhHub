72ebf43b75336d63e7ecb3383e1d5b8ba72beac6
Muhammad Wildan Amiinuddin <muhammad.wildan.ft21@mail.umy.ac.id>
2024-01-11 23:54:27 +0700

    add MurajaahUIState.kt

---
+++ app/src/main/java/com/example/tahfizhhub/ui/MurajaahUIState.kt
@@ -0,0 +1,45 @@
+package com.example.tahfizhhub.ui
+
+import com.example.tahfizhhub.AddEvent
+import com.example.tahfizhhub.model.MurajaahData
+
+data class AddMurajaahUIState(
+    val addMurajaahEvent: AddMurajaahEvent = AddMurajaahEvent()
+
+)
+
+data class AddMurajaahEvent(
+    var murajaahID: String = "",
+    val juz: String = "",
+    val surat: String = "",
+    val ayat: String = "",
+    val halaman: String = "",
+    var timestamp: Any? = null
+)
+
+fun AddMurajaahEvent.toMurajaahData() = MurajaahData(
+    murajaahID = murajaahID,
+    juz = juz,
+    surat = surat,
+    ayat = ayat,
+    halaman = halaman,
+    timestamp = timestamp
+)
+
+data class DetailMurajaahUIState(
+    val addMurajaahEvent: AddMurajaahEvent = AddMurajaahEvent()
+)
+
+fun MurajaahData.toDetailMurajaahData(): AddMurajaahEvent =
+    AddMurajaahEvent(
+        murajaahID = murajaahID,
+        juz = juz,
+        surat = surat,
+        ayat = ayat,
+        halaman = halaman,
+        timestamp = timestamp
+    )
+
+fun MurajaahData.toUIStateMurajaahData(): AddMurajaahUIState = AddMurajaahUIState(
+    addMurajaahEvent = this.toDetailMurajaahData()
+)
